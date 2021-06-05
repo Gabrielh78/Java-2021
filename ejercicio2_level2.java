5cafc059f3e5f11c3f0dd0a74319b27d7fe3a997
Gabrielh78 <68515945+Gabrielh78@users.noreply.github.com>
2021-06-05 14:43:57 -0300

    Add files via upload

--- ejercicio2_level2.java
+++ ejercicio2_level2.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 
 public class ejercicio2_level2 {
+    private static boolean add;
     public static void main(String[] args) {
         ArrayList<Integer> enteros = new ArrayList();
         enteros.add(2);
-        enteros.add(3);
+        add = enteros.add(3);
         enteros.add(4);
         enteros.add(5);
         enteros.add(6);
@@ -16,6 +17,8 @@ public class ejercicio2_level2 {
         System.out.println("Lista final");
         mostar(enteros);
         System.out.println("Tamano :" + enteros.size());
+    }
+    private static void mostrar(ArrayList<Integer> enteros) {
     }
     public static void mostar(ArrayList <Integer> array) {
         for (Integer item : array){
